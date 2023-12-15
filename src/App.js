1432848456343f73a5d8893fd7be1fc43236ae83
Phuc Hau <haunhpr024@gmail.com>
2023-12-15 14:12:32 +0700

    updated landing page desktop

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import WriteBlog from './Pages/WriteBlog';
 import Profile from './Pages/Profile';
 import EditUSer from './Pages/EditUser';
 import Search from './Pages/Search';
+import Desktop from './Components/LandingPageDesktop/Desktop';
 
 function App() {
     const user = useSelector((state) => state.auth.login?.currentUser);
@@ -23,52 +24,82 @@ function App() {
 
         return children;
     };
+
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    const [isMobile, setMobile] = useState(true);
+    useEffect(() => {
+        const handleSize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.addEventListener('resize', handleSize);
+        handleSize();
+        return () => window.removeEventListener('resize', handleSize);
+    }, []);
+    useEffect(() => {
+        if (windowSize.width > 500) {
+            setMobile(false);
+        } else {
+            setMobile(true);
+        }
+    }, [windowSize]);
     return (
         <Router>
             <div className="App content mx-auto max-w-[1110px] w-full px-6">
                 <Routes>
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/" element={<Login />} />
-                    <Route
-                        path="/home"
-                        element={
-                            <ProtectedRoutes>
-                                <Home />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route
-                        path="/write-blog"
-                        element={
-                            <ProtectedRoutes>
-                                <WriteBlog />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route
-                        path="/profile"
-                        element={
-                            <ProtectedRoutes>
-                                <Profile />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route
-                        path="/edit-user"
-                        element={
-                            <ProtectedRoutes>
-                                <EditUSer />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route
-                        path="/search"
-                        element={
-                            <ProtectedRoutes>
-                                <Search />
-                            </ProtectedRoutes>
-                        }
-                    />
+                    {isMobile ? (
+                        <>
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/" element={<Login />} />
+                            <Route
+                                path="/home"
+                                element={
+                                    <ProtectedRoutes>
+                                        <Home />
+                                    </ProtectedRoutes>
+                                }
+                            />
+                            <Route
+                                path="/write-blog"
+                                element={
+                                    <ProtectedRoutes>
+                                        <WriteBlog />
+                                    </ProtectedRoutes>
+                                }
+                            />
+                            <Route
+                                path="/profile"
+                                element={
+                                    <ProtectedRoutes>
+                                        <Profile />
+                                    </ProtectedRoutes>
+                                }
+                            />
+                            <Route
+                                path="/edit-user"
+                                element={
+                                    <ProtectedRoutes>
+                                        <EditUSer />
+                                    </ProtectedRoutes>
+                                }
+                            />
+                            <Route
+                                path="/search"
+                                element={
+                                    <ProtectedRoutes>
+                                        <Search />
+                                    </ProtectedRoutes>
+                                }
+                            />
+                        </>
+                    ) : (
+                        <Route path="/desktop" element={<Desktop />} />
+                    )}
                 </Routes>
             </div>
         </Router>
---
+++ src/Components/LandingPageDesktop/Desktop.jsx
@@ -0,0 +1,12 @@
+const Desktop = () => {
+    return (
+        <>
+            <h1 style={{ fontSize: '16px', fontWeight: '600' }}>
+                Not support on desktop . Please, convert to mobile use my app
+                ^_^
+            </h1>
+        </>
+    );
+};
+
+export default Desktop;
